f350ece7e0299a24a80a4b31ec110e1f8a5fe8cf
jonathan brigitte <jonathan.brigitteISN@gmail.com>
2014-05-21 16:14:15 +0200

    Une partie du menu pour le jeu

---
+++ menu3.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      eleve
+#
+# Created:     10/04/2014
+# Copyright:   (c) eleve 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+ans = True
+while ans:
+    print("""
+    1. Francais
+    2. Anglais
+
+    Appuyer sur entrer pour quitter
+    """)
+    ans=input("Choisissez une langue")
+    if ans == 1:
+        print("le jeux sera en version francaise")
+        execfile("francais.py")
+    elif ans == 2:
+        print("le jeux sera en version anglaise")
+        execfile("anglais.py")
